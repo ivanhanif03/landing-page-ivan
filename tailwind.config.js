67fa551e99ad53c12c79d5509c3d15684ccd9375
ivanhanif03 <ivanhanif03@gmail.com>
2022-12-20 17:31:07 +0700

    update my portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary : '#3b82f6',
+        secondary: '#64748b',
+        dark: '#0f172a',
+      },
+      screens: {
+        '2xl' : '1320px',
+      },
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'],
+     },
+     keyframes: {
+      wiggle: {
+        '0%, 100%': { transform: 'rotate(-3deg)' },
+        '50%': { transform: 'rotate(3deg)' },
+      },
+    },
+    },
+  },
+  plugins: [],
+}
